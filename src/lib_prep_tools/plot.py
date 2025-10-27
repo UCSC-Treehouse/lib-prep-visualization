69c82ac9a5f4ed3b5a07574c278af44c4761fcf3
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2025-10-26 18:12:03 -0700

    feat: color_map is optional in plot config

--- src/lib_prep_tools/plot.py
+++ src/lib_prep_tools/plot.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, field_validator, model_validator
+from pydantic import BaseModel, Field, field_validator, model_validator
 import json
 from pathlib import Path
 import scanpy as sc
@@ -18,7 +18,7 @@ class ColorByConfig(BaseModel):
     """
     meta_key: str
     categories: list[str] = []
-    color_map: dict[str, str] = {}
+    color_map: dict[str, str] = Field(default_factory=dict)
     
     @field_validator("color_map")
     @classmethod
