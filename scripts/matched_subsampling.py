d25c281475efbfe9ec75d3c4b827ffcf417577de
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2025-12-01 10:11:39 -0800

    feat: validate that the metadata label is present in all metadata tables

--- scripts/matched_subsampling.py
+++ scripts/matched_subsampling.py
@@ -95,10 +95,22 @@ def load_metadata(base_dir: Path, compendia_id: str) -> pd.DataFrame:
     return metadata_df
 
 
+def validate_metadata_labels(meta_df_dict: dict[str, pd.DataFrame], label: str) -> bool:
+    """
+    Validate that the specified metadata label exists in all metadata DataFrames. If any DataFrame is missing the label, return False.
+    """
+    for meta_df in meta_df_dict.values():
+        if label not in meta_df.columns:
+            return False
+    return True
+
+
 def main():
     config_fp, data_dir = parse_args()
     ms_config = load_ms_config(data_dir, config_fp)
     meta_df_dict = {comp_id: load_metadata(data_dir, comp_id) for comp_id in ms_config.compendia_set}
+    if not validate_metadata_labels(meta_df_dict, ms_config.metadata_label):
+        raise ValueError(f"Metadata label '{ms_config.metadata_label}' not found in all compendia metadata.")
     return
 
 if __name__ == "__main__":
