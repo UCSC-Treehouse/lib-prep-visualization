9cd7f75a96c7c1bc28a35487b990e8ee07ba1111
pRottinghuis <70864925+pRottinghuis@users.noreply.github.com>
2025-12-01 22:07:25 -0800

    test: test cases for three compendia compute_min_sample_counts

--- tests/test_matched_subsampling.py
+++ tests/test_matched_subsampling.py
@@ -30,6 +30,11 @@ metadata_5 = pd.DataFrame({
     "disease": ["A", "B", "B", "B"],
 })
 
+metadata_7 = pd.DataFrame({
+    "sample_id": ["S21", "S22", "S23", "S24"],
+    "age": [1, 2, 3, 4],
+})
+
 
 def test_two_compendia_matching_entries():
     """
@@ -40,10 +45,7 @@ def test_two_compendia_matching_entries():
         "compendia_2": metadata_2,
     }
     result = compute_min_sample_counts(meta_df_dict, "disease")
-    expected = {
-        "A": 2,
-        "B": 2,
-    }
+    expected = {"A": 2, "B": 2}
     assert result == expected
 
 def test_two_compendia_with_one_unique_entry():
@@ -55,8 +57,56 @@ def test_two_compendia_with_one_unique_entry():
         "compendia_3": metadata_3,
     }
     result = compute_min_sample_counts(meta_df_dict, "disease")
-    expected = {
-        "A": 1,
-        "B": 2,
+    expected = {"A": 1, "B": 2}
+    assert result == expected
+
+def test_two_compendia_no_matching_entries():
+    """
+    Test when both compendia have completely different disease entries.
+    """
+    meta_df_dict = {
+        "compendia_1": metadata_1,
+        "compendia_4": metadata_4,
     }
+    result = compute_min_sample_counts(meta_df_dict, "disease")
+    expected = {}
+    assert result == expected
+
+def test_three_compendia_matching_entries():
+    """
+    Test with three compendia having some overlapping disease entries. 'A' only appears once in metadata_5.
+    """
+    meta_df_dict = {
+        "compendia_1": metadata_1,
+        "compendia_2": metadata_2,
+        "compendia_5": metadata_5,
+    }
+    result = compute_min_sample_counts(meta_df_dict, "disease")
+    expected = {"A": 1, "B": 2}
+    assert result == expected
+
+def test_three_compendia_one_unique_entry():
+    """
+    Test with three compendia where one has a unique disease entry not found in the others. 'C' only appears in metadata_3.
+    """
+    meta_df_dict = {
+        "compendia_1": metadata_1,
+        "compendia_2": metadata_2,
+        "compendia_3": metadata_3,
+    }
+    result = compute_min_sample_counts(meta_df_dict, "disease")
+    expected = {"A": 1, "B": 2}
+    assert result == expected
+
+def test_three_compendia_one_unique_compendia():
+    """
+    Test with three compendia where one has completely different disease entries. Result should be empty.
+    """
+    meta_df_dict = {
+        "compendia_1": metadata_1,
+        "compendia_2": metadata_2,
+        "compendia_4": metadata_4,
+    }
+    result = compute_min_sample_counts(meta_df_dict, "disease")
+    expected = {}
     assert result == expected
